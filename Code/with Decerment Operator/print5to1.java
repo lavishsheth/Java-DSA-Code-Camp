2d4a7a11877264b395e4d664b4c05976319b7072
Lavish Sheth <109017996+lavishsheth@users.noreply.github.com>
2024-01-20 12:53:20 +0530

    Delete Code directory

--- Code/with Decerment Operator/print5to1.java
+++
@@ -1,7 +0,0 @@
-public class print5to1 {
-public static void main(String args[]){
-    for(int i=5;i>=1;i--){
-        System.out.println(i);
-    }
-}    
-}
--- Code/with Decerment Operator/tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
-public class Table {
-public static void main(String args[]){
-    int  i=3;
-for(int j=10;j>=1;j--){
-System.out.println(i+"X"+j+"="+i*j);
-}
-}
-}
